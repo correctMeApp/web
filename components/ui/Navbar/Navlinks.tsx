bbcf3769cc839111eee2af52c31c2c51ff9b04dd
Eren Kabakçı <erenkabakci@gmail.com>
2024-03-18 00:09:24 +0100

    Download link and .dmg

--- components/ui/Navbar/Navlinks.tsx
+++ components/ui/Navbar/Navlinks.tsx
@@ -35,17 +35,24 @@ export default function Navlinks() {
           <Logo />
         </Link>
         <nav className="ml-6 space-x-2 lg:block">
-          <Link href="/pricing" className={s.link}>
+          <Link href="/pricing" className={`${s.link} mr-4 active:text-opacity-50`}>
             Pricing
           </Link>
+          <a 
+            href="/CorrectMe.dmg" 
+            download 
+            className={`${s.link} px-4 py-2 bg-slate-800 rounded-full active:text-opacity-50`}
+          >
+            Download the Mac App
+          </a>
         </nav>
       </div>
       <div className="flex justify-end space-x-8">
-        <Link href="/account" className={s.link}>
+        <Link href="/account" className={`${s.link} active:text-opacity-50`}>
           Account
         </Link>
         {isLoggedIn && (
-          <button onClick={handleSignOut} className={s.link}>
+          <button onClick={handleSignOut} className={`${s.link} active:text-opacity-50`}>
           Sign out
         </button>
         )}
