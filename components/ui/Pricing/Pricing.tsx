9dfb9ea33bb01fab7896a49550b5ca1b128c9380
Eren Kabakçı <erenkabakci@gmail.com>
2024-04-02 00:45:34 +0200

    Pricing card update

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -4,8 +4,8 @@ import Button from '@/components/ui/Button';
 import { getStripe } from '@/utils/stripe/client';
 import { getErrorRedirect } from '@/utils/helpers';
 import cn from 'classnames';
-import { useRouter, usePathname } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { Price, Product, PricingPlanInterval } from '@/app/types/models';
 import { useAuth } from '@/app/authContext';
 
@@ -27,13 +27,16 @@ export default function Pricing({ product, prices }: Props) {
     useState<PricingPlanInterval>(PricingPlanInterval.Year);
   const [priceIdLoading, setPriceIdLoading] = useState<string>();
   const currentPath = usePathname();
+  const searchParams = useSearchParams()
+  const checkoutPriceId = searchParams.get('priceId');
 
   const handleStripeCheckout = async (price: Price) => {
     setPriceIdLoading(price.id);
   
     if (!isLoggedIn) {
       setPriceIdLoading(undefined);
-      return router.replace('/auth/signin?redirect=pricing');
+      const redirectUrl = `pricing?priceId=${price.id}`;
+      return router.replace(`/auth/signin?redirect=${redirectUrl}`);
     }
 
     const userResponse = await fetch('/api/user/profile', {
@@ -81,6 +84,17 @@ export default function Pricing({ product, prices }: Props) {
     setPriceIdLoading(undefined);
   };
 
+
+  useEffect(() => {
+    if (checkoutPriceId) {
+      const price = prices.find(price => price.id === checkoutPriceId);
+  
+      if (price) {
+        handleStripeCheckout(price);
+      }
+    }
+  }, [checkoutPriceId, prices, handleStripeCheckout]);
+
   if (!product) {
     return (
       <section className="bg-slate-900">
@@ -121,7 +135,7 @@ export default function Pricing({ product, prices }: Props) {
                     billingInterval === 'month'
                       ? 'relative w-1/2 bg-gray-800 border-zinc-800 shadow-sm text-white'
                       : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 sm:w-auto sm:px-8`}
                 >
                   Monthly
                 </button>
@@ -134,7 +148,7 @@ export default function Pricing({ product, prices }: Props) {
                     billingInterval === 'year'
                       ? 'relative w-1/2 bg-gray-800 border-zinc-800 shadow-sm text-white'
                       : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+                  } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 sm:w-auto sm:px-8`}
                 >
                   Yearly
                 </button>
@@ -157,17 +171,17 @@ export default function Pricing({ product, prices }: Props) {
                 <div
                   key={product.id}
                   className={cn(
-                    'flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-gray-800',
+                    'flex flex-col rounded-lg shadow-sm divide-y divide-zinc-600 bg-slate-800',
                     'flex-1', // This makes the flex item grow to fill the space
                     'basis-1/3', // Assuming you want each card to take up roughly a third of the container's width
-                    'max-w-xs' // Sets a maximum width to the cards to prevent them from getting too large
+                    'max-w-md', // Sets a maximum width to the cards to prevent them from getting too large
                   )}
                 >
                   <div className="p-6">
                     <h2 className="text-2xl font-semibold leading-6 text-white">
-                      {product.name}
+                      Duck it!
                     </h2>
-                    <p className="mt-4 text-zinc-300">{product.description}</p>
+                    <p className="mt-4 text-zinc-300">Single macOS app to solve all your writing problems</p>
                     <p className="mt-8">
                       <span className="text-5xl font-extrabold white">
                         {priceString}
@@ -185,6 +199,8 @@ export default function Pricing({ product, prices }: Props) {
                     >
                       {'Try for free'}
                     </Button>
+
+                    <p className="mt-4 text-zinc-300 text-center font-bold"><b>No credit cards needed during the trial period</b></p>
                   </div>
                 </div>
               );
