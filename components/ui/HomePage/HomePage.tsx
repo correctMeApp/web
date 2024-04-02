e6b93573e906791d37dca07f3a084eb101a360c4
Eren Kabakçı <erenkabakci@gmail.com>
2024-04-02 14:47:04 +0200

    Limit faq max width

--- components/ui/HomePage/HomePage.tsx
+++ components/ui/HomePage/HomePage.tsx
@@ -13,14 +13,6 @@ export default function HomePage() {
           <h1><i>Duck it!</i> away all your writing <span className={styles.under}>troubls</span>.</h1>
           <p>Duck it! is your instant AI writing assistant. Whenever you need to improve grammar, concise text, and translate with just one shortcut.</p>
           <p>
-          <Link href="/#download">
-            <span>Download free trial</span>
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="16" fill="none">
-                <path fill="#fff" d="M8.857 2.556C8.322 3.21 7.416 3.69 6.695 3.69c-.087 0-.164-.01-.219-.021a1.34 1.34 0 0 1-.032-.284c0-.83.426-1.65.873-2.174C7.897.535 8.868.032 9.677 0c.021.087.032.208.032.317 0 .83-.36 1.638-.852 2.239Zm.568 1.31c.459 0 2.097.044 3.167 1.595-.087.065-1.725.983-1.725 3.036 0 2.38 2.075 3.222 2.14 3.244-.01.054-.327 1.146-1.092 2.271-.688.972-1.408 1.966-2.49 1.966-1.092 0-1.376-.633-2.62-.633-1.224 0-1.661.655-2.655.655-.994 0-1.693-.917-2.479-2.031C.743 12.647 0 10.605 0 8.66 0 5.559 2.02 3.91 4.008 3.91c1.06 0 1.933.688 2.6.688.633 0 1.616-.732 2.817-.732Z" style={{fill:'#fff', fillOpacity:1}} />
-              </svg>
-            </span>
-        </Link>
           </p>
           <div id="faq" ></div>
         </div>
@@ -29,7 +21,7 @@ export default function HomePage() {
           <div className={styles.headline}>
               <h2 className={styles.title}>Why? <i>Duck it!</i></h2>
           </div>
-          <div className={styles.features} style={{ marginTop: '40px', paddingBottom: '40px'}}>
+          <div className={styles.features} style={{ marginTop: '40px', paddingBottom: '40px', maxWidth: '1500px'}}>
           <div>
             <p className={styles.title}>"I already have X extension on my browser and Y subscription."</p>
             <p className={styles.desc}>Duck it! works anywhere. Wherever you have an input field, you can use Duck it! A single macOS app that works on your browser, mail composer, or notes... you name it. Say goodbye to multiple apps.</p>
